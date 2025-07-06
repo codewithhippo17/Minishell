d6d2310d60be78bdbc72efcb4c675d9704a687af
ehamza <elhaiba.hamza17@gmail.com>
2025-07-07 00:59:07 +0100

    livring all to yassin

---
+++ includes/heredoc.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehamza <ehamza@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 23:25:21 by ehamza            #+#    #+#             */
+/*   Updated: 2025/07/06 23:25:53 by ehamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEREDOC_H
+# define HEREDOC_H
+
+# include "types.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+# define LEN_RANDOM 8
+
+#endif
