3e2673fe40c4fb19514621d7f52146437f92b687
ehamza <elhaiba.hamza17@gmail.com>
2025-07-07 03:19:15 +0100

    random function done

--- heredoc/random.c
+++ heredoc/random.c
@@ -6,8 +6,41 @@
 /*   By: ehamza <ehamza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 22:59:33 by ehamza            #+#    #+#             */
-/*   Updated: 2025/07/06 22:59:34 by ehamza           ###   ########.fr       */
+/*   Updated: 2025/07/07 01:08:12 by ehamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/heredoc.h"
+#include <stdio.h>
+#include <unistd.h>
+# define LEN_RANDOM 8
+char	*ft_random(void)
+{
+	int		random_fd;
+	char	*filename;
+	char	*buffer;
+	int		i;
+	char	*charset;
+
+	i = 0;
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	random_fd = open("/dev/urandom", O_RDONLY);
+	if (random_fd < 0)
+		return (perror("open /dev/urandom"), NULL);
+	if (read(random_fd, buffer, LEN_RANDOM) != LEN_RANDOM)
+		return (perror("open /dev/urandom"), NULL);
+	filename = malloc(LEN_RANDOM);
+	if (!filename)
+		return (perror("malloc"), NULL);
+	while (i < LEN_RANDOM)
+	{
+		filename[i] = charset[buffer[i] % sizeof(charset)];
+        i++;
+	}
+    return (filename);
+}
+
+int main()
+{
+    printf("%s\n", ft_random());
+}
