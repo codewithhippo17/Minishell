9d73a46fd5faa32c574217e0f407331389ce97dd
Your Name <elhaiba.hamza17@gmail.com>
2025-07-15 18:15:03 +0100

    start concepting expantion to be continued
    -add notes
    -enhance the script printer

--- test/main.c
+++ test/main.c
@@ -28,6 +28,45 @@ void	handle_main_signal(int sig)
 	}
 }
 
+const char *flag_to_string(t_flag flag)
+{
+    static const char *flag_names[] = {
+        "WORD", "CMD", "ARG", "WS", "PIPE",
+        "LR", "RR", "DLR", "DRR", "HEREDOC",
+        "VAR", "ES", "ERROR"
+    };
+
+    if (flag < 0 || flag > ERROR)
+        return "UNKNOWN";
+    return flag_names[flag];
+}
+
+const char *quote_to_string(t_quote quote)
+{
+    static const char *flag_names[] = {
+        	"NQS", "SQS", "DQS", "UQS"
+    };
+        if (quote < 0 || quote > UQS)
+            return "UNKNOWN";
+    return flag_names[quote];
+}
+
+void print_tokens(t_token *token)
+{
+    t_token *curr = token;
+
+    printf("Lexer Output:\n    ");
+    while (curr)
+    {
+        printf("[\e[1;34m{\"%s\"}\e[0m: (%s) (%s)]", curr->value, flag_to_string(curr->type), quote_to_string(curr->quote));
+        if (curr->next)
+            printf("\033[32;1m---->\033[0m");
+        curr = curr->next;
+    }
+    printf("\n\n");
+}
+
+
 int	main(int argc, char *argv[], char **env)
 {
 	t_minishell	*minishell;
@@ -61,103 +100,10 @@ int	main(int argc, char *argv[], char **env)
 			add_history(input);
 			tokens = lexer(input);
 			checker(&tokens, minishell);
+            print_tokens(tokens);
 		}
 	}
 	return (EXIT_SUCCESS);
 }
 
-/*void	print_tokens(t_token *token)
-{
-	t_token		*curr;
-	t_token		*hhh;
-	const char	*type_str;
-	const char	*quote_str;
 
-	curr = token;
-	hhh = NULL;
-	printf("Lexer Output:\n    ");
-	while (curr)
-	{
-		printf("[%s]", curr->value);
-		if (curr->next)
-		{
-			printf("->");
-		}
-		hhh = curr;
-		curr = curr->next;
-	}
-	printf("\nhhhhhhhhh\n");
-	while (hhh)
-	{
-		printf("[%s]", hhh->value);
-		if (hhh->prev)
-			printf("<-");
-		hhh = hhh->prev;
-	}
-	printf("\n\n");
-	// Summary
-	printf("Summary:\n");
-	while (token)
-	{
-		// Token type string representation
-		switch (token->type)
-		{
-		case CMD:
-			type_str = "CMD";
-			break ;
-		case ARG:
-			type_str = "ARG";
-			break ;
-		case VAR:
-			type_str = "VAR";
-			break ;
-		case PIPE:
-			type_str = "PIPE";
-			break ;
-		case RR:
-			type_str = "RR";
-			break ;
-		case DRR:
-			type_str = "DRR";
-			break ;
-		case LR:
-			type_str = "LR";
-			break ;
-		case DLR:
-			type_str = "DLR";
-			break ;
-		case ES:
-			type_str = "ES";
-			break ;
-		case WS:
-			type_str = "WS";
-			break ;
-		case WORD:
-			type_str = "WORD";
-			break ;
-		case ERROR:
-			type_str = "ERROR";
-			break ;
-		case HEREDOC:
-			type_str = "HEREDOC";
-			break ;
-		}
-		switch (token->quote)
-		{
-		case NQS:
-			quote_str = "NQS";
-			break ;
-		case SQS:
-			quote_str = "SQS";
-			break ;
-		case DQS:
-			quote_str = "DQS";
-			break ;
-		case UQS:
-			quote_str = "UQS";
-			break ;
-		}
-		printf("    [%-12s] → %-10s | %s\n", token->value, type_str, quote_str);
-		token = token->next;
-	}
-}*/
