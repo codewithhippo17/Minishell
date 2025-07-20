847836bbefcb2e75f2aa701f8902c7007c194fc3
Your Name <elhaiba.hamza17@gmail.com>
2025-07-20 02:52:54 +0100

    inhancement but still

--- expander/ft_expander.c
+++ expander/ft_expander.c
@@ -48,86 +48,87 @@ static bool	no_var(char *value)
 	return (true);
 }
 
-char *char_to_str(char c)
+char	*char_to_str(char c)
 {
-    char *str = malloc(2);
-    if (!str)
-        return NULL;
-    str[0] = c;
-    str[1] = '\0';
-    return str;
+	char	*str;
+
+	str = malloc(2);
+	if (!str)
+		return (NULL);
+	str[0] = c;
+	str[1] = '\0';
+	return (str);
 }
 
 t_token	*nonvar_parser(char *str, int *i)
 {
 	char	*tmp;
-    int idx;
+	int		idx;
+	t_token	*token;
 
-    idx = 0;
-    if (!str || !str[idx])
-        return NULL;
-    tmp = ft_strdup("");
-    while (str[idx])
-    {
-        if (str[idx] == '$')
-        {
-            if (str[idx] && str[idx + 1] && str[idx + 1] == '?')
-            {
-                tmp = ft_strjoin(tmp, ft_strdup("127"));
-                idx += 2; // Skip the $? part
-            }
-            else if (str[*i] && ft_isdigit(str[*i + 1]))
-            {
-                tmp = ft_strjoin(tmp, ft_substr(str, *i, 2));
-                idx++;
-            }
-            else
-                break; // Stop if we encounter a variable
-        }
-        else
-        {
-            tmp = ft_strjoin(tmp, char_to_str(str[idx]));
-        }
-        idx++;
-    }
-    t_token *token = malloc(sizeof(t_token));
-    token->value = tmp;
-    token->type = WORD;
-    token->quote = NQS;
-    token->join = J;
-    token->next = NULL;
-    token->prev = NULL;
-    token->hd = NULL;
-    *i += idx; // Update index to skip processed characters
-    return token;
+	idx = 0;
+	if (!str || !str[idx])
+		return (NULL);
+	tmp = ft_strdup("");
+	while (str[idx])
+	{
+		if (str[idx] == '$')
+		{
+			if (str[idx] && str[idx + 1] && str[idx + 1] == '?')
+			{
+				tmp = ft_strjoin(tmp, ft_strdup("127"));
+				idx += 2;
+			}
+			else if (str[*i] && ft_isdigit(str[*i + 1]))
+			{
+				tmp = ft_strjoin(tmp, ft_substr(str, *i, 2));
+				idx++;
+			}
+			else
+				break ;
+		}
+		else
+			tmp = ft_strjoin(tmp, char_to_str(str[idx]));
+		idx++;
+	}
+	token = malloc(sizeof(t_token));
+	token->value = tmp;
+	token->type = WORD;
+	token->quote = NQS;
+	token->join = J;
+	token->next = NULL;
+	token->prev = NULL;
+	token->hd = NULL;
+	*i += idx;
+	return (token);
 }
 
 char	*get_varname(char *str, int *i)
 {
-    int		len;
-    char	*varname;
+	int		len;
+	char	*varname;
 
-    len = get_var_len(&str[1]);
-    if (len == 0)
-        return (NULL);
-    varname = malloc(len + 1);
-    if (!varname)
-        return (NULL);
-    ft_strlcpy(varname, &str[1], len + 1);
-    *i += len + 1; // Move index past the variable name
-    return (varname);
+	len = get_var_len(&str[1]);
+	if (len == 0)
+		return (NULL);
+	varname = malloc(len + 1);
+	if (!varname)
+		return (NULL);
+	ft_strlcpy(varname, &str[1], len + 1);
+	*i += len + 1;
+	return (varname);
 }
 
 void	ft_exspliter(t_token **token, char *str, int idx,
 		t_minishell *minishell)
 {
-    t_splited   *splited;
-    t_token     *inner_head;
-    t_token     *inner_tail;
-    int         i;
-    t_token     *c;
+	t_splited	*splited;
+	t_token		*inner_head;
+	t_token		*inner_tail;
+	int			i;
+	t_token		*c;
 
-    i = 0;
+	i = 0;
 	c = *token;
 	inner_head = NULL;
 	inner_tail = NULL;
@@ -136,17 +137,15 @@ void	ft_exspliter(t_token **token, char *str, int idx,
 	while (str[i])
 	{
 		if (str[i] != '$')
-		{
 			append_token(&inner_head, &inner_tail, nonvar_parser(&str[i], &i));
-		}
 		else
 		{
-			splited = ft_spliter(my_getenv(get_varname(&str[i], &i),minishell->m_env));
-            append_token(&inner_head, &inner_tail, splited->head);
-            inner_tail = splited->tail;
-        }
+			splited = ft_spliter(my_getenv(get_varname(&str[i], &i),
+						minishell->m_env));
+			append_token(&inner_head, &inner_tail, splited->head);
+			inner_tail = splited->tail;
+		}
 	}
-    print_tokens(inner_head);
 }
 
 void	ft_expander(t_token **token, t_minishell *minishell)
