92ca5cf57cc9b32b3f51636b2945bf3483c0a73f
Your Name <elhaiba.hamza17@gmail.com>
2025-08-01 02:03:54 +0100

    feat(signals and memory management): build the file structure

---
+++ includes/signals.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elhaiba hamza <ehamza@student.1337.ma>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 01:30:06 by elhaiba hamza     #+#    #+#             */
+/*   Updated: 2025/08/01 01:53:16 by elhaiba hamza    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef SIGNALS_H
+# define SIGNALS_H
+
+# include <signal.h>
+
+extern volatile sig_atomic_t g_received_signal;
+
+#endif
